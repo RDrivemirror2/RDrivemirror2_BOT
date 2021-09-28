3cd5ce2749abebc71ae1b6fd2ddb2e7012920081
RDrivemirror2 <90256011+RDrivemirror2@users.noreply.github.com>
2021-09-28 12:13:24 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -154,9 +154,9 @@ help_string_telegraph = f'''<br>
 <b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
 '''
 help = Telegraph(access_token=telegraph_token).create_page(
-        title='Slam Mirrorbot Help',
-        author_name='Slam Mirrorbot',
-        author_url='https://github.com/SlamDevs/slam-mirrorbot',
+        title='RDrive Mirrorbot Help',
+        author_name='RDrive Mirror',
+        author_url='https://t.me/RDriveMirror',
         html_content=help_string_telegraph,
     )["path"]
 
